bbd5161b4231bd74a35b54131059aeb2d8af034e
floss-order <flossOrder@gmail.com>
2020-12-29 22:57:18 +0300

    Set up SCSS automatic imports

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,23 @@ const fs = require('fs')
 const { src, dest, parallel, series, watch } = require('gulp')
 const sass = require('gulp-sass')
 sass.compiler = require('sass')
+const autoImports = require('gulp-auto-imports')
 
 
-const SRC_PATH = path.join(__dirname, 'src')
-const BUILD_PATH = path.join(__dirname, 'build')
+const SRC_PATH = 'src'
+const BUILD_PATH = 'build'
 const ASSETS_PATH = path.join(BUILD_PATH, 'assets')
+
+function autoImport() {
+    return src(path.join(SRC_PATH, 'blocks/*.scss'))
+    .pipe(autoImports({ 
+        format: '@import "$path";',
+        dest: SRC_PATH,
+        fileName: '_blocks.scss',
+        retainOrder: true,
+    }))
+    .pipe(dest(SRC_PATH))
+}
 
 function compileSCSS() {
     return src(path.join(SRC_PATH, '**/*.scss'))
@@ -16,3 +28,4 @@ function compileSCSS() {
 }
 
 exports.compileSCSS = compileSCSS
+exports.default = autoImport
